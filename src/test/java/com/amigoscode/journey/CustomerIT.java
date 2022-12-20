7dbd6ade251f35806608a0f2af8ef1377f21f84e
amigoscode <amigoscode@gmail.com>
2022-12-20 23:57:39 +0000

    Packaging Jar, Maven surefire and failsafe plugins

--- src/test/java/com/amigoscode/journey/CustomerIT.java
+++ src/test/java/com/amigoscode/journey/CustomerIT.java
@@ -21,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-public class CustomerIntegrationTest {
+public class CustomerIT {
 
     @Autowired
     private WebTestClient webTestClient;
