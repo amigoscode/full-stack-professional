a4833911db41daa5734c15cb1bfc46d9cd8d2ff5
amigoscode <amigoscode@gmail.com>
2023-01-04 01:07:56 +0000

    add counter to PingPongController

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingPongController {
 
+    private static int COUNTER = 0;
+
     record PingPong(String result){}
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("Pong");
+        return new PingPong("Pong: %s".formatted(++COUNTER));
     }
 }
