b328130b8d5d0264e843c58400b4f5232103d210
amigoscode <amigoscode@gmail.com>
2023-01-16 23:39:12 +0000

    Change link color to blue

--- frontend/react/src/components/shared/SideBar.jsx
+++ frontend/react/src/components/shared/SideBar.jsx
@@ -113,7 +113,7 @@ const NavItem = ({icon, children, ...rest}) => {
                 role="group"
                 cursor="pointer"
                 _hover={{
-                    bg: 'red.400',
+                    bg: 'blue.400',
                     color: 'white',
                 }}
                 {...rest}>
