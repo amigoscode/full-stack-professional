fef88833369fc77a17347b42c69a1144093d1977
BOUALI Ali <bouali.pro@gmail.com>
2023-02-20 14:44:43 -0800

    Revert "Angular app"

--- backend/src/main/java/com/amigoscode/auth/LoginRequest.java
+++
@@ -1,7 +0,0 @@
-package com.amigoscode.auth;
-
-public record LoginRequest(
-        String username,
-        String password
-) {
-}
--- frontend/angular/src/app/models/authentication-request.ts
+++
@@ -1,4 +0,0 @@
-export interface AuthenticationRequest {
-  username?: string;
-  password?: string;
-}
