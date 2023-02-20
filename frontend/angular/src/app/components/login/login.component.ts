de4ed6a98b6251800cf52f56fbc447751d7678ff
alibouali <ali.bouali@mimacom.com>
2023-02-20 17:07:43 +0100

    login servie / model

--- frontend/angular/src/app/components/login/login.component.ts
+++ frontend/angular/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationRequest } from '../../models/authentication-request';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  authenticationRequest: AuthenticationRequest = {};
 }
---
+++ frontend/angular/src/app/models/authentication-request.ts
@@ -0,0 +1,4 @@
+export interface AuthenticationRequest {
+  username?: string;
+  password?: string;
+}
