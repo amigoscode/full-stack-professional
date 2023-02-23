6d7d0a5e75365667e5b0b35d120c229520799e0e
ali-bouali <bouali.pro@gmail.com>
2023-02-23 15:05:38 +0100

    header + logout

--- frontend/angular/src/app/components/header-bar/header-bar.component.ts
+++ frontend/angular/src/app/components/header-bar/header-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { AuthenticationResponse } from '../../models/authentication-response';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-bar',
@@ -7,6 +9,11 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./header-bar.component.scss']
 })
 export class HeaderBarComponent {
+
+  constructor(
+    private router: Router
+  ) {
+  }
   items: Array<MenuItem> = [
     {
       label: 'Profile',
@@ -21,8 +28,34 @@ export class HeaderBarComponent {
     },
     {
       label: 'Sign out',
-      icon: 'pi pi-sign-out'
+      icon: 'pi pi-sign-out',
+      command: () => {
+        localStorage.clear();
+        this.router.navigate(['login']);
+      }
     },
   ];
+
+  get username(): string {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      const authResponse: AuthenticationResponse = JSON.parse(storedUser);
+      if (authResponse && authResponse.customerDTO && authResponse.customerDTO.username) {
+        return authResponse.customerDTO.username;
+      }
+    }
+    return '--';
+  }
+
+  get userRole(): string {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      const authResponse: AuthenticationResponse = JSON.parse(storedUser);
+      if (authResponse && authResponse.customerDTO && authResponse.customerDTO.roles) {
+        return authResponse.customerDTO.roles[0];
+      }
+    }
+    return '--';
+  }
 
 }
