59d8ea3f800ce2a2371f06e63fc7ffe8beec39d7
amigoscode <amigoscode@gmail.com>
2023-03-18 21:10:36 +0000

    tidy up angular files and fix links

--- frontend/angular/src/app/app-routing.module.ts
+++ frontend/angular/src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
     path: 'login',
     component: LoginComponent
   },
